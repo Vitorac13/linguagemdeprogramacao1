895d83d278451fae67fd2298c60e1a1e5875eaf0
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-08-24 23:10:24 +0000

    adiciona classe Pessoa com atributos e construtores

--- Aula01_Classes/Exemplo01/Pessoa.java
+++ Aula01_Classes/Exemplo01/Pessoa.java
@@ -1,3 +1,19 @@
 public class Pessoa {
-    
+    private String nome;
+    private String sexo;
+    private String cpf;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, String sexo, String cpf, LocalDate dataNascimento){
+        this.nome = nome;
+        this.sexo = sexo;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+
+    LocalDate d = new LocalDate(now());
+    Pessoa p = new Pessoa("Vitor", "M", "12321123321", d);
 }
